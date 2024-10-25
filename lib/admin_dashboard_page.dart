778251f1a81adb21d47d8066136140468ca12502
Dima-Haj <dimahaj218@gmail.com>
2024-10-26 00:17:42 +0300

    UI adjustments (fonts, sizes), added Employee Management and Delivery Management pages, fixed navigation bar issues

--- lib/admin_dashboard_page.dart
+++ lib/admin_dashboard_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'customer_management_page.dart';
+import 'delivery_management_page.dart';
+import 'employee_management_page.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:url_launcher/url_launcher.dart'; // for launching URLs
-import 'customer_management_page.dart'; // Import CustomerManagementPage
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(const AdminDashboardApp());
 
@@ -12,97 +15,207 @@ class AdminDashboardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AdminDashboardPage(),
+      home: AdminDashboard(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class AdminDashboardPage extends StatelessWidget {
-  const AdminDashboardPage({super.key});
+class AdminDashboard extends StatefulWidget {
+  const AdminDashboard({super.key});
+
+  @override
+  _AdminDashboardState createState() => _AdminDashboardState();
+}
+
+class _AdminDashboardState extends State<AdminDashboard> {
+  int _selectedIndex = 1; // Default index for dashboard
+
+  final List<Widget> _pages = [
+    const CustomerManagementPage(),
+    const AdminDashboardPageContent(),
+    const DeliveryManagementPage(),
+    const EmployeeManagementPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Admin Dashboard'),
-        backgroundColor: Colors.blueAccent,
-        leading: PopupMenuButton<String>(
-          onSelected: (value) {
-            switch (value) {
-              case 'Notifications':
-                break;
-              case 'Drivers Information':
-                break;
-              case 'Delivery Reports':
-                break;
-              // Add more cases as needed
-            }
-          },
-          itemBuilder: (BuildContext context) {
-            return [
-              const PopupMenuItem(
-                value: 'Notifications',
-                child: Text('Notifications History'),
-              ),
-              const PopupMenuItem(
-                value: 'Drivers Information',
-                child: Text('Drivers Information'),
-              ),
-              const PopupMenuItem(
-                value: 'Delivery Reports',
-                child: Text('Delivery Reports'),
-              ),
-              const PopupMenuItem(
-                value: 'Settings',
-                child: Text('Settings'),
-              ),
-            ];
-          },
-        ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
       ),
-      body: const SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            // Delivery Overview
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Delivery Overview',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ),
-            DeliveryOverviewWidget(),
-            // Performance Metrics
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Performance Metrics',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ),
-            PerformanceMetricsWidget(),
-            // Map with clickable icon
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Live Map',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ),
-            MapWidget(),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 22),
+            label: 'Customers',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard, size: 22),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_shipping, size: 22),
+            label: 'Deliveries',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people, size: 22),
+            label: 'Employees',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color.fromARGB(255, 131, 107, 81),
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        selectedLabelStyle: const TextStyle(fontSize: 10),
+        unselectedLabelStyle: const TextStyle(
+            fontSize: 9, color: Color.fromARGB(255, 120, 120, 120)),
       ),
     );
   }
 }
 
-// Make _launchURL a global method
-Future<void> _launchURL(String url) async {
-  if (!await launchUrl(Uri.parse(url))) {
-    throw 'Could not launch $url';
+class AdminDashboardPageContent extends StatelessWidget {
+  const AdminDashboardPageContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox.expand(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: const AssetImage('assets/images/image1.png'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.65),
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+            child: SingleChildScrollView(
+              physics: const ClampingScrollPhysics(),
+              padding: const EdgeInsets.only(top: 80.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Delivery Overview',
+                      style: GoogleFonts.exo2(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const DeliveryOverviewWidget(),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Performance Metrics',
+                      style: GoogleFonts.exo2(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const PerformanceMetricsWidget(),
+                  Padding(
+                    padding: const EdgeInsets.all(19.0),
+                    child: Text(
+                      'Live Map',
+                      style: GoogleFonts.exo2(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const MapWidget(),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 38.0,
+          left: 16.0,
+          child: Container(
+            color: const Color.fromARGB(0, 216, 214, 214),
+            padding: const EdgeInsets.all(8.0),
+            child: PopupMenuButton<String>(
+              icon: const Icon(Icons.more_horiz, color: Colors.white),
+              onSelected: (value) {
+                switch (value) {
+                  case 'Notifications':
+                    break;
+                  case 'Drivers Information':
+                    break;
+                  case 'Delivery Reports':
+                    break;
+                }
+              },
+              itemBuilder: (BuildContext context) {
+                return [
+                  PopupMenuItem<String>(
+                    value: 'Notifications',
+                    child: Container(
+                      color: Colors.transparent,
+                      child: const Text(
+                        'Notifications History',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  PopupMenuItem<String>(
+                    value: 'Drivers Information',
+                    child: Container(
+                      color: Colors.transparent,
+                      child: const Text(
+                        'Drivers Information',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  PopupMenuItem<String>(
+                    value: 'Delivery Reports',
+                    child: Container(
+                      color: Colors.transparent,
+                      child: const Text(
+                        'Delivery Reports',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  PopupMenuItem<String>(
+                    value: 'Settings',
+                    child: Container(
+                      color: Colors.transparent,
+                      child: const Text(
+                        'Settings',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ];
+              },
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
 
@@ -120,7 +233,7 @@ class DeliveryOverviewWidget extends StatelessWidget {
         boxShadow: const [
           BoxShadow(
             color: Colors.black12,
-            blurRadius: 10,
+            blurRadius: 15,
             spreadRadius: 5,
           ),
         ],
@@ -132,19 +245,22 @@ class DeliveryOverviewWidget extends StatelessWidget {
             status: "Active",
             icon: Icons.local_shipping,
             count: 12,
-            iconColor: Colors.green, // Random number for Active
+            iconColor: const Color.fromARGB(255, 141, 126, 106),
+            iconSize: 30.0,
           ),
           DeliveryStatusCard(
             status: "Pending",
             icon: Icons.access_time,
             count: 5,
-            iconColor: Colors.orange, // Random number for Pending
+            iconColor: const Color.fromARGB(255, 141, 126, 106),
+            iconSize: 30.0,
           ),
           DeliveryStatusCard(
             status: "Completed",
             icon: Icons.check_circle,
-            count: 24,
-            iconColor: Colors.blue, // Random number for Completed
+            count: 30,
+            iconColor: const Color.fromARGB(255, 141, 126, 106),
+            iconSize: 30.0,
           ),
         ],
       ),
@@ -157,6 +273,7 @@ class DeliveryStatusCard extends StatelessWidget {
   final IconData icon;
   final int count;
   final Color iconColor;
+  final double iconSize;
 
   const DeliveryStatusCard({
     super.key,
@@ -164,13 +281,13 @@ class DeliveryStatusCard extends StatelessWidget {
     required this.icon,
     required this.count,
     required this.iconColor,
+    this.iconSize = 30.0,
   });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        // Navigate to CustomerManagementPage
         Navigator.push(
           context,
           MaterialPageRoute(
@@ -184,14 +301,14 @@ class DeliveryStatusCard extends StatelessWidget {
             children: [
               Icon(
                 icon,
-                size: 40,
+                size: iconSize,
                 color: iconColor,
               ),
               const SizedBox(width: 8),
               Text(
                 count.toString(),
-                style: const TextStyle(
-                    fontSize: 24, fontWeight: FontWeight.bold),
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ],
           ),
@@ -211,7 +328,6 @@ class PerformanceMetricsWidget extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 16.0),
       child: const Column(
         children: <Widget>[
-          // First Row (Delay Rate and On-Time Rate)
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
@@ -220,7 +336,6 @@ class PerformanceMetricsWidget extends StatelessWidget {
             ],
           ),
           SizedBox(height: 10),
-          // Second Row (Success Rate and Avg Cost per Delivery)
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
@@ -247,7 +362,7 @@ class MetricCard extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         margin: const EdgeInsets.symmetric(horizontal: 4.0),
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: const Color.fromARGB(255, 255, 255, 255),
           borderRadius: BorderRadius.circular(10),
           boxShadow: const [
             BoxShadow(
@@ -266,8 +381,7 @@ class MetricCard extends StatelessWidget {
             const SizedBox(height: 10),
             Text(
               value,
-              style: const TextStyle(
-                  fontSize: 20, fontWeight: FontWeight.bold),
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ],
         ),
@@ -276,6 +390,7 @@ class MetricCard extends StatelessWidget {
   }
 }
 
+@override
 class MapWidget extends StatelessWidget {
   const MapWidget({super.key});
 
@@ -286,27 +401,20 @@ class MapWidget extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Stack(
         children: [
-          FlutterMap(
-            options: const MapOptions(
-              initialCenter: LatLng(35.217018, 31.771959),
-              initialZoom: 13.0,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: FlutterMap(
+              options: MapOptions(
+                initialCenter: LatLng(35.217018, 31.771959),
+                initialZoom: 13.0,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+              ],
             ),
-            children: [
-              // Tile Layer for OpenStreetMap
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName: 'com.example.app',
-              ),
-              // Rich Attribution
-              RichAttributionWidget(
-                attributions: [
-                  TextSourceAttribution(
-                    'OpenStreetMap contributors',
-                    onTap: () => _launchURL('https://openstreetmap.org/copyright'),
-                  ),
-                ],
-              ),
-            ],
           ),
           Align(
             alignment: Alignment.topRight,
@@ -315,7 +423,9 @@ class MapWidget extends StatelessWidget {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const FullScreenMap()),
+                  MaterialPageRoute(
+                    builder: (context) => const FullScreenMap(),
+                  ),
                 );
               },
             ),
@@ -334,7 +444,7 @@ class FullScreenMap extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Full Screen Map')),
       body: FlutterMap(
-        options: const MapOptions(
+        options: MapOptions(
           initialCenter: LatLng(35.217018, 31.771959),
           initialZoom: 13.0,
         ),
@@ -343,16 +453,16 @@ class FullScreenMap extends StatelessWidget {
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             userAgentPackageName: 'com.example.app',
           ),
-          RichAttributionWidget(
-            attributions: [
-              TextSourceAttribution(
-                'OpenStreetMap contributors',
-                onTap: () => _launchURL('https://openstreetmap.org/copyright'),
-              ),
-            ],
-          ),
         ],
       ),
     );
+  }
+}
+
+Future<void> _launchURL(String url) async {
+  if (await canLaunchUrl(Uri.parse(url))) {
+    await launchUrl(Uri.parse(url));
+  } else {
+    throw 'Could not launch $url';
   }
 }
--- lib/stopwatch_widget.dart
+++ lib/stopwatch_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'dart:math'; // For sin, cos, and pi
+import 'package:google_fonts/google_fonts.dart'; // Google fonts package
 
 class StopwatchWidget extends StatefulWidget {
   const StopwatchWidget({super.key});
@@ -9,7 +10,8 @@ class StopwatchWidget extends StatefulWidget {
   _StopwatchWidgetState createState() => _StopwatchWidgetState();
 }
 
-class _StopwatchWidgetState extends State<StopwatchWidget> with SingleTickerProviderStateMixin {
+class _StopwatchWidgetState extends State<StopwatchWidget>
+    with SingleTickerProviderStateMixin {
   late Timer _timer;
   int _seconds = 0;
   bool _isRunning = false;
@@ -73,25 +75,35 @@ class _StopwatchWidgetState extends State<StopwatchWidget> with SingleTickerProv
   // Generate rotated and positioned rectangles around the circle
   List<Widget> _buildClockMarkers(double radius, double screenWidth) {
     List<Widget> markers = [];
-    double rectangleWidth = screenWidth * 0.022; // Rectangle width as 2.2% of screen width
-    double rectangleHeight = screenWidth * 0.07; // Rectangle height as 7% of screen width
-    Color lighterDarkGrey = const Color.fromARGB(255, 85, 85, 85); // Lighter dark grey for the rectangles
+    double rectangleWidth =
+        screenWidth * 0.015; // Rectangle width as 2.2% of screen width
+    double rectangleHeight =
+        screenWidth * 0.075; // Rectangle height as 7% of screen width
+    Color lighterDarkGrey = const Color.fromARGB(
+        255, 85, 85, 85); // Lighter dark grey for the rectangles
 
     for (int i = 0; i < 12; i++) {
-      double angle = (i * 30.0) * (pi / 180); // Convert degrees to radians (30 degrees for each hour marker)
+      double angle = (i * 30.0) *
+          (pi /
+              180); // Convert degrees to radians (30 degrees for each hour marker)
 
       markers.add(
         Transform(
           transform: Matrix4.identity()
-            ..translate(radius * cos(angle), radius * sin(angle)) // Adjust position
-            ..rotateZ(angle + pi / 2), // Rotate to align with the circle's radius
+            ..translate(
+                radius * cos(angle), radius * sin(angle)) // Adjust position
+            ..rotateZ(
+                angle + pi / 2), // Rotate to align with the circle's radius
           alignment: Alignment.center,
           child: Container(
-            width: rectangleWidth,  // Small width for the rectangles
+            width: rectangleWidth, // Small width for the rectangles
             height: rectangleHeight, // Height of the rectangles
             decoration: BoxDecoration(
-              color: _isShiftStarted ? Colors.green : lighterDarkGrey, // Initially lighter dark grey, green after shift starts
-              borderRadius: BorderRadius.circular(4.0), // Rounded corners for rectangles
+              color: _isShiftStarted
+                  ? Colors.green
+                  : lighterDarkGrey, // Initially lighter dark grey, green after shift starts
+              borderRadius:
+                  BorderRadius.circular(4.0), // Rounded corners for rectangles
             ),
           ),
         ),
@@ -111,26 +123,34 @@ class _StopwatchWidgetState extends State<StopwatchWidget> with SingleTickerProv
     // Circle sizes and radius based on screen size
     double outerCircleDiameter = screenWidth * 0.75; // 75% of screen width
     double innerCircleDiameter = screenWidth * 0.85; // Inner glowing circle
-    double radius = outerCircleDiameter / 2.0 * 0.8; // Adjust the radius for clock markers
+    double radius =
+        outerCircleDiameter / 2.0 * 0.8; // Adjust the radius for clock markers
 
     // Adjust font sizes based on screen size
-    double timeFontSize = screenWidth * 0.12; // 12% of screen width
+    double timeFontSize = screenWidth * 0.10; // 12% of screen width
     double labelFontSize = screenWidth * 0.05; // 5% of screen width
 
     // Colors for the elements
-    Color lighterDarkGrey = const Color.fromARGB(255, 85, 85, 85); // Lighter dark grey for the rectangles
-    Color evenLighterGrey = const Color.fromARGB(255, 120, 120, 120); // Lighter grey for the inner circle background
-    Color darkGrey = const Color.fromARGB(255, 55, 55, 55); // Dark grey for the outer border
+    Color lighterDarkGrey = const Color.fromARGB(
+        255, 85, 85, 85); // Lighter dark grey for the rectangles
+    Color evenLighterGrey = const Color.fromARGB(
+        255, 120, 120, 120); // Lighter grey for the inner circle background
+    Color darkGrey =
+        const Color.fromARGB(255, 55, 55, 55); // Dark grey for the outer border
 
     return Center(
       child: GestureDetector(
-        onTap: _isShiftStarted ? _resetStopwatch : _startStopwatch, // Start/Reset on tap
+        onTap: _isShiftStarted
+            ? _resetStopwatch
+            : _startStopwatch, // Start/Reset on tap
         child: AnimatedBuilder(
           animation: _controller,
           builder: (context, child) {
             // 3D flip effect on the Y-axis
-            double rotationValue = _controller.value * pi; // Rotation in radians
-            bool isFlipped = _controller.value > 0.5; // Flip complete after halfway
+            double rotationValue =
+                _controller.value * pi; // Rotation in radians
+            bool isFlipped =
+                _controller.value > 0.5; // Flip complete after halfway
 
             return Transform(
               transform: Matrix4.rotationY(rotationValue),
@@ -144,7 +164,8 @@ class _StopwatchWidgetState extends State<StopwatchWidget> with SingleTickerProv
                     height: innerCircleDiameter,
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
-                      color: darkGrey.withOpacity(0.9), // Lighter grey background
+                      color:
+                          darkGrey.withOpacity(0.8), // Lighter grey background
                     ),
                   ),
 
@@ -155,8 +176,11 @@ class _StopwatchWidgetState extends State<StopwatchWidget> with SingleTickerProv
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       border: Border.all(
-                        color: _isShiftStarted ? Colors.green : lighterDarkGrey, // Turn green after shift starts
-                        width: screenWidth * 0.025, // 2.5% of screen width for border width
+                        color: _isShiftStarted
+                            ? Colors.green
+                            : lighterDarkGrey, // Turn green after shift starts
+                        width: screenWidth *
+                            0.015, // 2.5% of screen width for border width
                       ),
                     ),
                   ),
@@ -174,19 +198,32 @@ class _StopwatchWidgetState extends State<StopwatchWidget> with SingleTickerProv
                       children: [
                         Text(
                           _formatTime(_seconds),
-                          style: TextStyle(
-                            fontSize: timeFontSize, // Scaled font size
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                          style: GoogleFonts.exo2(
+                            textStyle: TextStyle(
+                              fontSize: timeFontSize, // Scaled font size
+
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
-                        SizedBox(height: screenHeight * 0.02), // Space based on screen height
+                        SizedBox(
+                            height: screenHeight *
+                                0.02), // Space based on screen height
                         Text(
-                          _isShiftStarted ? 'End Shift' : 'Start Shift', // Change text based on shift state
-                          style: TextStyle(
-                            fontSize: labelFontSize, // Scaled font size for label
-                            fontWeight: FontWeight.bold,
-                            color: _isShiftStarted ? Colors.red : Colors.green, // Start Shift in green, End Shift in red
+                          _isShiftStarted
+                              ? 'End Shift'
+                              : 'Start Shift', // Change text based on shift state
+                          style: GoogleFonts.exo2(
+                            textStyle: TextStyle(
+                              fontSize:
+                                  labelFontSize, // Scaled font size for label
+                              fontWeight: FontWeight.bold,
+                              color: _isShiftStarted
+                                  ? Colors.red
+                                  : Colors
+                                      .green, // Start Shift in green, End Shift in red
+                            ),
                           ),
                         ),
                       ],
